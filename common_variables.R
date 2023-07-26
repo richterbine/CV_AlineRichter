85f863c1eab25855440e6b9a22c4a78923150626
Aline Richter <linebio.r@gmail.com>
2023-07-26 11:33:09 -0300

    initial commit

---
+++ common_variables.R
@@ -0,0 +1,15 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Doutora em Ecologia, tenho experiência com coleta e organização de dados biológicos com ênfase em invertebrados terrestres (borboletas), análises e programação em R, incluindo visualização de dados, manipulação de grande conjunto de dados e manipulação de objetos espaciais. Tenho boa comunicação, sou uma pessoa proativa e com facilidade para trabalhos em equipe."
+
+
+# Language skills for skill bars visualization
+
+skills <- tibble::tribble(
+  ~skill,               ~level,
+  "R",                  5,
+  "ggplot",             5,
+  "QGIS",               4.5,
+  "Git",                4
+)
